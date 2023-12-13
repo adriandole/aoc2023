fb318a9b2d50d45f3b15c66812f12dda1dbf870c
Adrian Dole <adrian@dole.tech>
2023-12-13 13:34:15 -1000

    Simplification

--- app/Day8.hs
+++ app/Day8.hs
@@ -4,7 +4,7 @@ module Day8 where
 
 import Common
 import Data.Either
-import Data.HashMap.Strict as HashMap (HashMap, fromList, lookup, keys)
+import Data.HashMap.Strict as HashMap (HashMap, fromList, keys, lookup, (!))
 import Data.Text (pack)
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -24,43 +24,23 @@ pGraph = do
   where
     label = count 3 letterChar
 
-stepsToZ :: GraphNodes -> String -> Int -> String -> Int
-stepsToZ nodes path steps startNode =
-  if startNode !! 2 == 'Z'
+stepsToMatch :: (String -> Bool) -> GraphNodes -> String -> Int -> String -> Int
+stepsToMatch matcher nodes path steps startNode =
+  if matcher startNode
     then steps
-    else case HashMap.lookup startNode nodes of
-      Nothing -> error $ "Invalid node: " ++ startNode
-      Just (l, r) -> case head path of
-        'L' -> stepsToZ nodes (tail path) (steps + 1) l
-        'R' -> stepsToZ nodes (tail path) (steps + 1) r
-        _ -> error "Invalid instruction"
-
-multiWalk :: Int -> GraphNodes -> [String] -> String -> ([String], Int)
-multiWalk steps nodes startNodes path =
-  if all (\n -> n !! 2 == 'Z') startNodes
-    then (startNodes, steps)
     else
-      multiWalk
-        (steps + 1)
-        nodes
-        ( map
-            ( \n -> case HashMap.lookup n nodes of
-                Nothing -> error $ "Invalid node: " ++ n
-                Just (l, r) -> case head path of
-                  'L' -> l
-                  'R' -> r
-            )
-            startNodes
-        )
-        (tail path)
+      let (l, r) = nodes ! startNode
+          next = case head path of
+            'L' -> l
+            'R' -> r
+       in stepsToMatch matcher nodes (tail path) (steps + 1) next
 
 day8 = do
   f <- readFile "input/day8.txt"
   let (path, nodes) = fromRight (error "Parse error") (parse pGraph "day8.txt" (pack f))
-  let (_, p1) = multiWalk 0 nodes ["AAA"] (cycle path)
-  let steps = map (stepsToZ nodes (cycle path) 0) (filter (\n -> n !! 2 == 'A') (keys nodes))
+  let p1 = stepsToMatch (== "ZZZ") nodes (cycle path) 0 "AAA"
+  let steps = map (stepsToMatch (\n -> n !! 2 == 'Z') nodes (cycle path) 0) (filter (\n -> n !! 2 == 'A') (keys nodes))
   let p2 = foldl lcm 1 steps
   --   parseTest pGraph (pack f)
   printf "Part 1: %d\n" p1
   printf "Part 2: %d\n" p2
---   printf "Part 2: %d\n" $ 0
